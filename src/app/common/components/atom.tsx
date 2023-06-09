d7258be906675bfd5865aa1018d73b651fbc57e6
YujiTakebuchi <takeyou.swift@gmail.com>
2023-06-10 00:53:14 +0900

    jotaiを使用して選択されたファイルのステート管理

---
+++ src/app/common/components/atom.tsx
@@ -0,0 +1,3 @@
+import { atom } from "jotai";
+
+export const optimizeFileListAtom = atom<Array<File>>([]);
--- src/app/common/components/index.tsx
+++ src/app/common/components/index.tsx
@@ -1,5 +1,26 @@
 "use client";
 
-export function FileListInput() {
-  return <input type="file" multiple />;
-}
+import { useAtom } from "jotai";
+import React, { useEffect } from "react";
+import { optimizeFileListAtom } from "./atom";
+
+export const FileListInput: React.FC = () => {
+  const [fileList, setFileList] = useAtom(optimizeFileListAtom);
+  useEffect(() => {
+    console.log(fileList);
+  }, [fileList]);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const enteredFileListObject = e.currentTarget.files;
+    const enteredFileListObjectLength = enteredFileListObject?.length;
+    if (!enteredFileListObjectLength) return;
+    const enteredFileList = new Array(enteredFileListObjectLength)
+      .fill(null)
+      .map((_, idx) => enteredFileListObject[idx]);
+    console.log(enteredFileList);
+    console.log(typeof enteredFileList);
+    setFileList(enteredFileList);
+  };
+
+  return <input type="file" multiple onChange={handleChange} />;
+};
